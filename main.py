fe6c24909df43db90878bcecb884456ea011e800
Василий Кузнецов <vasya1679@mail.ru>
2022-02-08 13:26:50 +0300

    feat: добавлен вывод сообщения, в зависимости от пройденной дистанции

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ distance = constants.STEPS * step_length / 1000
 velocity = distance / constants.TIME
 calories = 0.035 * constants.WEIGHT + velocity ** 2/ constants.HEIGHT * 0.029 * constants.WEIGHT
 print(f'Distance: {distance} km Calories: {calories} ccal')
+if (distance < 2):
+    print("Недостаточная дистанция!")
+elif (distance > 2 and distance < 4):
+    print("Уже лучше")
+else :
+    print("Норма выполнена!")
